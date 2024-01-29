0f6dd65a67ababe0801ef46acacfc57144a6964c
OsoTega <96957781+OsoTega@users.noreply.github.com>
2024-01-29 22:49:49 +0000

    first commit

---
+++ src/components/MaxWidth.jsx
@@ -0,0 +1,12 @@
+import { cn } from '@/lib/utils'
+import React, { ReactNode } from 'react'
+
+const MaxWidth = ({className,children}) => {
+    return (
+        <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
+            {children}
+        </div>
+    )
+}
+
+export default MaxWidth
